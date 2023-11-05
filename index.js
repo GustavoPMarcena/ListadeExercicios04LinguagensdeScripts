ad9ef305888ba77afe6bef1103f448a12506109a
GustavoPMarcena <gustavopmarcena@outlook.com>
2023-11-05 18:04:30 -0300

    feat: localstorage implementado

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import { adicionar, remover, marcar, desmarcar, listar, verificarID} from "./lista.js"
+import { adicionar, remover, marcar, desmarcar, listar, verificarID } from "./lista.js"
 
 const inputButton = document.getElementById("inputButton");
 const parentTable = document.querySelector("#listId");
+
+document.onload = carregarPagina();
 
 inputButton.addEventListener("click", () => {
     const nome = document.getElementById("itemName").value;
     const preco = document.getElementById("itemCost").value;
 
-    if(nome && preco) {
+    if (nome && preco) {
         adicionarItem(nome, preco);
 
         document.getElementById("itemName").value = '';
         document.getElementById("itemCost").value = '';
     }
-    
+
 })
 
 function adicionarItem(nome, preco) {
     let numeroAleatorio = Math.floor(Math.random() * 10000);
     if (verificarID(numeroAleatorio)) return adicionarItem(nome, preco); // Caso o id se repita, vai gerar novamente
     const novoObjeto = {
-        id:numeroAleatorio,
-        nome: nome, 
+        id: numeroAleatorio,
+        nome: nome,
         preco: preco,
-        comprado:false
+        comprado: false
     }
     adicionar(novoObjeto);
     recarregarLista();
@@ -31,7 +33,7 @@ function adicionarItem(nome, preco) {
 
 function recarregarLista() {
     parentTable.replaceChildren();
-    listar().forEach((element, index) => {
+    listar().forEach((element) => {
         const nomeTable = document.createElement("td");
         nomeTable.textContent = element.nome;
         const precoTable = document.createElement("td");
@@ -43,7 +45,6 @@ function recarregarLista() {
 
         // Função para marcar a checkbox
         checkBox.addEventListener("change", (e) => {
-            console.log("aaaaaaaaaaa");
             (e.target.checked) ? marcar(element) : desmarcar(element);
             recarregarLista();
         })
@@ -59,7 +60,7 @@ function recarregarLista() {
         }
         const removerButtonTd = document.createElement("td");
         removerButtonTd.appendChild(removerButton);
-        
+
         const novaTable = document.createElement("tr");
 
         novaTable.appendChild(nomeTable);
@@ -68,13 +69,25 @@ function recarregarLista() {
         novaTable.appendChild(removerButtonTd);
 
         parentTable.appendChild(novaTable);
-
-
-
-
-
     });
+    salvarDados();
+
     console.log("Função dentro do recarregar lista");
     console.log(listar());
-    
+
+}
+
+
+function salvarDados() {
+    localStorage.setItem("list", JSON.stringify(listar()));
+}
+
+function carregarPagina() {
+    let listaCompras = JSON.parse(localStorage.getItem("list"));
+    if (listaCompras && listaCompras.length > 0) {
+        listaCompras.forEach((element) => {
+            adicionar(element);
+        });
+        recarregarLista();
+    }
 }
